1527c35b28847d6d0a039953d77ffc007d59ae7a
Yeonmin Lee <rx7554@gmail.com>
2025-09-15 16:11:10 +0900

    lym

--- Basic constants and printing/Basic constants and printing/main.cpp
+++ Basic constants and printing/Basic constants and printing/main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	printf("=== Basic Constants ===\n");
-	printf("Name\t: %s\n", "Yeonmin");
-	printf("Initial\t: %c\n", 'Y');
-	printf("Age\t: %d\n", 23);
-	printf("Score\t: %.6f\n", 93.5);
-	printf("Count\t: %u\n", 3000000000U);
+	printf("=== Basic Constants ===\n\n");
+	printf("Name\t: %s\n\n", "Yeonmin");
+	printf("Initial\t: %c\n\n", 'Y');
+	printf("Age : %d\n\n", 23);
+	printf("Score\t: %.6f\n\n", 93.5);
+	printf("Count\t: %u\n\n", 3000000000U);
 	return 0;
 }
