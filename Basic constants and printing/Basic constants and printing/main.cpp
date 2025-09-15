ebb8256a7339bd4ef98903e9801f8f57185fb677
Yeonmin Lee <rx7554@gmail.com>
2025-09-15 15:57:48 +0900

    lym

---
+++ Basic constants and printing/Basic constants and printing/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("=== Basic Constants ===\n");
+	printf("Name\t: %s\n", "Yeonmin");
+	printf("Initial\t: %c\n", 'Y');
+	printf("Age\t: %d\n", 23);
+	printf("Score\t: %.6f\n", 93.5);
+	printf("Count\t: %u\n", 3000000000U);
+	return 0;
+}
--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-	printf("Hello, World!\n");
+	printf("=== Basic Constants ===\n");
+	printf("Name\t: %s\n", "Yeonmin");
+	printf("Initial\t: %c\n", 'Y');
+	printf("Age\t: %d\n", 23);
+	printf("Score\t: %.6f\n", 93.5);
+	printf("Count\t: %u\n", 3000000000U);
 	return 0;
 }
