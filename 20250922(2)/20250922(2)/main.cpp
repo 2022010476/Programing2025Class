1b186f05fbaf6f9ed2b52b6d8b1111496529ef04
Yeonmin Lee <rx7554@gmail.com>
2025-09-22 16:20:49 +0900

    lym

--- 20250922(2)/20250922(2)/main.cpp
+++ 20250922(2)/20250922(2)/main.cpp
@@ -3,15 +3,15 @@
 int main(void)
 {
     char ch = 'A';
-    const char* msg = "First line\n\tIndented second line\nThird line";
+    const char* msg = "First line\n\n\tIndented second line\n\nThird line";
 
     double avg = 87.3456;
 
-    printf("=== Char/String & Escapes ===\n");
-    printf("Char : %c, code: %d\n", ch, ch);
-    printf("Message:\n%s\n", msg);
-    printf("Quotes: \"double quotes\" and 'single quotes'\n");
-    printf("Backslash: \\\n");
+    printf("=== Char/String & Escapes ===\n\n");
+    printf("Char : %c, code: %d\n\n", ch, ch);
+    printf("Message:\n\n%s\n\n", msg);
+    printf("Quotes: \"double quotes\" and 'single quotes'\n\n");
+    printf("Backslash: \\\n\n");
     printf("Average: [%8.2f]\n", avg);
 
     return 0;
