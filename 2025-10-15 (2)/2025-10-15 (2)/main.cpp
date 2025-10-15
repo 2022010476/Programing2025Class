4b0662746db0ce2ba0c972fdd2d83aea972d8d47
Yeonmin Lee <rx7554@gmail.com>
2025-10-15 09:25:50 +0900

    lym

---
+++ 2025-10-15 (2)/2025-10-15 (2)/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int chest;
+	char size;
+	
+	printf("가슴둘레(cm)를 입력하세요: ");
+	scanf_s("%d", &chest);
+
+	if (chest <= 90)
+	{
+		size = 'S';
+	}
+	else if (chest <= 100)
+	{
+		size = 'M';
+	}
+	else
+	{
+		size = 'L';
+	}
+	printf("사이즈는 %c입니다. \n", size);
+
+	return 0;
+}
