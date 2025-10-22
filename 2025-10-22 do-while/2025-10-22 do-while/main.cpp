ce9d0df5324ac8ca54f75077ab90da5a1553767b
Yeonmin Lee <rx7554@gmail.com>
2025-10-22 09:28:23 +0900

    lym

---
+++ 2025-10-22 do-while/2025-10-22 do-while/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, x, y, temp, gcd, lcm;
+
+	printf("두 정수를 입력하세요: ");
+	scanf_s("%d %d", &a, &b);
+
+	x = a;
+	y = b;
+
+	do
+	{
+		temp = x % y;
+		x = y;
+		y = temp;
+	} while (y != 0);
+
+	gcd = x;
+	lcm = (a * b) / gcd;
+
+	printf("최대공약수(GCD): %d\n", gcd);
+	printf("최소공배수(LCB): %d\n", lcm);
+
+	return 0;
+}
