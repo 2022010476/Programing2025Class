4114f6f02cdc7719443f4a3fc6ff41edc97853bd
Yeonmin Lee <rx7554@gmail.com>
2025-10-27 17:58:43 +0900

    lym

---
+++ 2025-10-27 (3.)/2025-10-27 (3.)/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int ref_func(int n);
+
+int main(void)
+{
+    int n, res;
+    scanf_s("%d", &n);
+    res = ref_func(n);
+    printf("%d\n", res);
+    return 0;
+}
+
+int ref_func(int n)
+{
+    int sum = 0, i;
+    for (i = 1; i <= n; i++)
+        sum += i;
+    return sum;
+}
